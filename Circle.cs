2de13a2b30c4a8174ea4d62e1bc5d33eb4f11776
samantha mazzola <smazzola7@gmail.com>
2018-08-01 15:11:06 -0400

    extra lab 10 need to finish

---
+++ Circle.cs
@@ -0,0 +1,60 @@
+﻿using System;
+namespace Lab10
+{
+    public class Circle //Base Class
+    {
+
+        //fields //private lowercase
+        private double radius
+
+
+        //properties //{get;set;}
+        public double Radius
+        {
+            set { radius = value; }
+            get { return radius; }
+
+        }
+
+        //constructors
+        //default
+        //overload constructor
+        public Circle()
+        {
+            Radius = 60;
+        }
+        public Circle(double r)
+        {
+        Radius = r;
+        }
+
+        //methods
+        public double CalculateCircumference()
+        {
+            double Circumfernence = Math.PI * 2 * Radius;
+            return Circumfernence;
+        }
+        public string CalculateFormattedCircumference() //rounded to the nearest 2 decimals, return that value as a string
+        {
+            Console.WriteLine($"");
+
+        }
+        public double CalculateArea()
+        {
+            double Area = Math.PI * (Radius*Radius);
+            Console.WriteLine($"");
+
+        }
+        public string CalculateFormattedArea() //rounded to the nearest 2 decimals, return that value as a string
+        {
+            Console.WriteLine($"");
+
+        }
+        private string FormatNumber(double x) // you can take in a double from the CalculateArea(), and CalculateCircumference() and pass a string which is used to round
+        {
+            
+            Console.WriteLine("");
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lab10
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Circle Tester!");
+
+
+
+
+        }
+    }
+}
